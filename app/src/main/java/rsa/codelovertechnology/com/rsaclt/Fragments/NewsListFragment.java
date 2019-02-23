7442190e0175c50f9fc4a91e5eb4a81b14fd6d03
Rahul Kumar <rahulinbscit@live.com>
2019-02-23 12:56:25 +0530

    Code Updated

--- app/src/main/java/rsa/codelovertechnology/com/rsaclt/Fragments/NewsListFragment.java
+++ app/src/main/java/rsa/codelovertechnology/com/rsaclt/Fragments/NewsListFragment.java
@@ -61,295 +61,8 @@ public class NewsListFragment  extends Fragment {
         super.onCreate(savedInstanceState);
 
         listModelList = new ArrayList<NewsListModel>();
-        listModelList.add(new NewsListModel(
-                1,
-                "Text", 5,
-                "This is Text Api Calling",
-                "This is Sub text Heading by Rahul Kumar ",
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                "Delhi",
-                20,
-                "Rahul Kumar",
-                "31-12-2018",
-                "Rahul Kuar",
-                "30-12-2018",
-                true,
-                1,
-                1,
-                "National",
-                "National"));
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        listModelList.add(new NewsListModel(
-                        1,
-                        "Text", 3,
-                        "This is Text 23 Api Calling",
-                        "This is Sub text Heading by Rahul Kumar ",
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        null,
-                        "Delhi Mumbai",
-                        20,
-                        "Rahul Kumar",
-                        "31-12-2018",
-                        "Rahul Kuar",
-                        "30-12-2018",
-                        true,
-                        1,
-                        1,
-                        "National",
-                        "National"
-                )
-        );
-        /*if(listModelList.size()==0) {
+
+        if(listModelList.size()==0) {
             Handler handler = new Handler();
             handler.postDelayed(new Runnable() {
                 @Override
@@ -357,11 +70,11 @@ public class NewsListFragment  extends Fragment {
                     listModelList = GetNewsList();
                 }
             }, 3000);
-        }*/
+        }
 
     }
 
-  /*  public List<NewsListModel> GetNewsList()
+   public List<NewsListModel> GetNewsList()
     {
         final List<NewsListModel> listNewsWebApiData = new ArrayList<NewsListModel>();
         Retrofit retrofit = new Retrofit.Builder()
@@ -374,7 +87,7 @@ public class NewsListFragment  extends Fragment {
             @Override
             public void onResponse(Call<List<NewsListModel>> call, Response<List<NewsListModel>> response) {
                 if (!response.isSuccessful()) {
-                    *//*text_View_Result.setText("Code : " + response.code());*//*
+//                    text_View_Result.setText("Code : " + response.code());
                 }
                 List<NewsListModel> NewsList = response.body();
                 Log.d("NewsList  " , "NewsList : " + NewsList);
@@ -387,9 +100,9 @@ public class NewsListFragment  extends Fragment {
             @Override
             public void onFailure(Call<List<NewsListModel>> call, Throwable t) {
                 Log.d("onFailure  " , "getMessage() : " + t.getMessage());
-                *//*text_View_Result.setText(t.getMessage());*//*
+//                text_View_Result.setText(t.getMessage());
             }
         });
         return listNewsWebApiData;
-    }*/
+    }
 }
